5e6d1855de8cdcdc5217a1293c5b76e3b456ee33
n00b87 <n00bskillz87@gmail.com>
2024-12-11 13:58:00 -0500

    Changed references of rcbasic binary to rcbasic4

--- dist_tool/main.cpp
+++ dist_tool/main.cpp
@@ -557,9 +557,9 @@ bool dist_desktop(int dist_os, int arch)
         string dfile_src = appendFileToPath(OS_DIST_DIR, dfile);
         string dfile_dst = appendFileToPath(DIST_OUTPUT_DIR, dfile);
 
-        if(dfile.compare("rcbasic")==0)
+        if(dfile.compare("rcbasic4")==0)
             dfile_dst = appendFileToPath(DIST_OUTPUT_DIR, PROJECT_OUTDIR_NAME);
-        else if(dfile.compare("rcbasic.exe")==0)
+        else if(dfile.compare("rcbasic4.exe")==0)
             dfile_dst = appendFileToPath(DIST_OUTPUT_DIR, PROJECT_OUTDIR_NAME + ".exe");
         //cout << "binaries: " << dfile_src << " <---> " << dfile_dst << endl;
 
@@ -709,7 +709,7 @@ int main(int argc, char * argv[])
     if(!parse(args))
         return 1;
 
-    string build_cmd = "rcbasic_build \"" + appendFileToPath(PROJECT_DIR, SOURCE) + "\"";
+    string build_cmd = "rcbasic_build4 \"" + appendFileToPath(PROJECT_DIR, SOURCE) + "\"";
 
     if(SOURCE.length() < 5)
     {
